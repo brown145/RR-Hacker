328de4ff6f9fcc4706fd1c8a915d3b91e9d90356
Scott Brown <brown145@gmail.com>
2013-04-05 09:23:08 -0700

    updates to use new pickles

--- js/common.js
+++ js/common.js
@@ -48,7 +48,50 @@ if(jTable.find('tr').length > 2) {
 // fire old charing modifications
 if ( document.location.pathname === '/rrportal/sliceDiceReporting.jsp' || document.location.pathname === '/rrportal/detailPage.jsp' ) {
 
-	setTimeout(function(){
-		graphZoom.init();
+    setTimeout(function(){
+        graphZoom.init();
+    }, 100);
+} else if ( document.location.pathname === '/rrportal/multivariateTests.jsp' ) {
+
+    setTimeout(function(){
+        MVTHistoryLinks.init();
     }, 100);
 }
+
+//for all pages
+setTimeout(function(){
+    var pageId, pageName ='error';
+
+        switch(document.location.pathname){
+            case '/rrportal/placements.jsp':
+                pageId = 28;
+                pageName = 'placements';
+                break;
+            case '/rrportal/urlPatterns.jsp':
+                pageId = 35;
+                pageName = 'Site Config Settings';
+                break;
+            case '/rrportal/pageAreas.jsp':
+                pageId = 29;
+                pageName = 'page areas';
+                break;
+            case '/rrportal/layouts.jsp':
+                pageId = 27;
+                pageName = 'layouts';
+                break;
+            case '/rrportal/attributePlaceholders.jsp':
+                pageId = 30;
+                pageName = 'attribute placeholders';
+                break;
+            case '/rrportal/strategyPage.jsp':
+                pageId = 32;
+                pageName = 'strategy messages';
+                break;
+
+
+        }
+
+        if(pageId){
+            publishStamp.init(pageId, pageName);
+        }
+    }, 100);
